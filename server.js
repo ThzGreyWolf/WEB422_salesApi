5d5806783c4c9e04b9c1a8fd177427343686ff2e
Kirushanth <thzgreywolf@gmail.com>
2020-06-11 12:21:05 -0400

    + api routes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const dataService = require("./modules/data-service.js");
 
-const myData = dataService("Your MongoDB Connection String Goes Here");
+const myData = dataService("mongodb+srv://thzgreywolf:yoitskiru@cluster0-emqvm.mongodb.net/sample_supplies?retryWrites=true&w=majority");
 
 const app = express();
 
@@ -16,23 +16,49 @@ const HTTP_PORT = process.env.PORT || 8080;
 // ************* API Routes
 
 // POST /api/sales (NOTE: This route must read the contents of the request body)
-
-
+app.post("/api/sales", (req, res) => {
+    myData.addNewSale(req.body).then((msg) => {
+        req.json({message: msg});
+    }).catch((err) => {
+        res.json({message: `Error: ${err}`});
+    });
+});
 
 // GET /api/sales (NOTE: This route must accept the numeric query parameters "page" and "perPage", ie: /api/sales?page=1&perPage=5 )
-
-
+app.get("/api/sales/:page&:perPage", (req, res) => {
+    myData.getAllSales(req.params.page, req.params.perPage).then((msg) => {
+        req.json({message: msg});
+    }).catch((err) => {
+        res.json({message: `Error: ${err}`});
+    });
+});
 
 // GET /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8)
-
-
+app.get("/api/sales/:id", (req, res) => {
+    myData.getSaleById(req.params.id).then((msg) => {
+        req.json({message: msg});
+    }).catch((err) => {
+        res.json({message: `Error: ${err}`});
+    });
+});
 
 // PUT /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8 as well as read the contents of the request body)
-
-
+app.put("/api/sales/:id", (req, res) => {
+    myData.updateSaleById(req.body, req.params.id).then((msg) => {
+        res.json({message: msg});
+    }).catch((err) => {
+        res.json({message: `Error: ${err}`});
+    });
+});
 
 // DELETE /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8)
-
+app.delete("/api/sales/:id", (req, res) => {
+    myData.deleteSaleById(req.params.id).then((msg) => {
+        res.json({message: msg});
+    }).catch((err) => {
+        res.json({message: `Error: ${err}`});
+    });
+});
 
 // ************* Initialize the Service & Start the Server
 
